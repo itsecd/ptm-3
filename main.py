51e80a64926fb002fc7092a279b8837cef0657f4
Santas7 <andreypanyshkin03@gmail.com>
2023-11-08 20:05:38 +0400

    создал базовые функции

--- main.py
+++ main.py
@@ -3,3 +3,44 @@ import re
 from checksum import calculate_checksum, serialize_result
 
 
+def validate_email(email):
+    pass
+
+
+def validate_height(height):
+    pass
+
+
+def validate_snils(snils):
+    pass
+
+
+def validate_passport(passport):
+    pass
+
+
+def validate_occupation(occupation):
+    pass
+
+
+def validate_longitude(longitude):
+    pass
+
+
+def validate_hex_color(hex_color):
+    pass
+
+
+def validate_issn(issn):
+    pass
+
+
+def validate_locale_code(locale_code):
+    pass
+
+
+def validate_time(time):
+    pass
+
+if __name__ == '__main__':
+    pass
