8d86072928e9472997d04c7de5413d3f6de2a4cb
pavel <tomashaytis@gmail.com>
2023-11-11 20:03:24 +0400

    Реализовал шаблоны для проверки номера телефона, http-статуса, ИНН, идентификатора и IP. Использовал флаг VERBOSE, улучшил читаемость написанных регулярных выражений. При проверке корректности IP учитывал верхнюю границу чисел - 255.

--- main.py
+++ main.py
@@ -20,16 +20,37 @@ def find_invalid_entries(dataframe: pd.DataFrame, column: str, pattern: str) -> 
         raise KeyError(f'Dataframe contains columns {", ".join(list(dataframe.keys()))}, but given column {column}')
     invalid_entries = []
     for i in range(len(dataframe[column])):
-        if not re.fullmatch(pattern, dataframe[column][i]):
+        if not re.fullmatch(pattern, dataframe[column][i], re.X):
             invalid_entries.append(i)
     return invalid_entries
 
 
 if __name__ == '__main__':
     dataset = pd.read_csv(CSV_FILE, sep=';', quotechar='"', encoding='utf-16')
+    row_numbers = []
     try:
-        row_numbers = find_invalid_entries(dataset, 'telephone', r'\+7-\(\d{3}\)-\d{3}-\d{2}-\d{2}')
-        print(calculate_checksum(row_numbers))
+        row_numbers += (find_invalid_entries(dataset, 'telephone', r'\+7-'
+                                                                   r'\(\d{3}\)-'
+                                                                   r'\d{3}-'
+                                                                   r'\d{2}-'
+                                                                   r'\d{2}'))
+        row_numbers += (find_invalid_entries(dataset, 'http_status_message', r'\d{3}'
+                                                                             r'\s'
+                                                                             r'.+'))
+        row_numbers += (find_invalid_entries(dataset, 'inn', r'\d{12}'))
+        row_numbers += (find_invalid_entries(dataset, 'identifier', r'\d{2}-'
+                                                                    r'\d{2}/'
+                                                                    r'\d{2}'))
+        row_numbers += (find_invalid_entries(dataset, 'ip_v4', r'(?: (?: 25[0-5]) | (?: 2[0-4]\d) | (?: 1?\d{,2}) )\.'
+                                                               r'(?: (?: 25[0-5]) | (?: 2[0-4]\d) | (?: 1?\d{,2}) )\.'
+                                                               r'(?: (?: 25[0-5]) | (?: 2[0-4]\d) | (?: 1?\d{,2}) )\.'
+                                                               r'(?: (?: 25[0-5]) | (?: 2[0-4]\d) | (?: 1?\d{,2}) )'))
+        # row_numbers += (find_invalid_entries(dataset, 'latitude', r'\d{2}-\d{2}/\d{2}'))
+        # row_numbers += (find_invalid_entries(dataset, 'blood_type', r'\d{2}-\d{2}/\d{2}'))
+        # row_numbers += (find_invalid_entries(dataset, 'isbn', r'\d{2}-\d{2}/\d{2}'))
+        # row_numbers += (find_invalid_entries(dataset, 'uuid', r'\d{2}-\d{2}/\d{2}'))
+        # row_numbers += (find_invalid_entries(dataset, 'date', r'\d{2}-\d{2}/\d{2}'))
+        print(len(row_numbers))
     except KeyError as error:
         logging.error(error)
 
