7ab1b390372b9079712dcbf938481f6a1d044325
artyEZ <abitur.arty@gmail.com>
2023-11-14 23:44:44 +0400

    change regex for occupation. change csv file and sync forks

--- csv_validation.py
+++ csv_validation.py
@@ -9,7 +9,7 @@ VALIDATION_RULES = {
     'height': r'\d\.\d{2}',
     'inn': r'\d{12}',
     'passport': r'\d{2}\s\d{2}\s\d{6}',
-    'occupation': r'[\wа-яА-ЯёЁ-]+(\s[\wа-яА-ЯёЁ-]+)*',
+    'occupation': r'^[A-Za-zА-Яа-я-\s]+$',
     'latitude': r'-?(?:90(?:\.0+)?|[1-8]?\d(?:\.\d+)?)',
     'hex_color': r'#([a-f0-9]{6}|[a-f0-9]{3})',
     'issn': r'\d{4}-\d{4}',
@@ -44,7 +44,8 @@ def validate_csv(file_path: str, validation_rules: dict[str]) -> List[int]:
 
 if __name__ == '__main__':
     invalid_list = validate_csv(FILE_PATH, VALIDATION_RULES)
-
-    checksum = calculate_checksum(invalid_list)
-    serialize_result(variant=23, checksum=checksum)
+    
+    hash_sum = calculate_checksum(invalid_list)
+    serialize_result(variant=23, checksum=hash_sum)
+    
     
