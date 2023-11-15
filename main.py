ab5af04d5c017f614e3582781ade80a64a497c66
Pancake2021 <92122200+Pancake2021@users.noreply.github.com>
2023-11-15 14:09:04 +0400

    correction of 1 error

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import re
-from pandas import pd
+import pandas
 from checksum import calculate_checksum, serialize_result
 
 VALIDATION_RULES = {
-    'email_regex' : r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$',
-    'height_regex' : r'^\d+(\.\d+)?$',
-    'snils_regex': r'^\d{11}$',
-    'passport_regex': r'^\d{2}\s\d{2}\s\d{6}$',
-    'occupation_regex': r'^[А-Яа-я\s]+$',
-    'longitude_regex': r'^-?\d+(\.\d+)?$',
-    'hex_color_regex': r'^#[0-9A-Fa-f]{6}$',
-    'issn_regex': r'^\d{4}-\d{4}$',
-    'locale_code_regex': r'^[a-z]{2}-[a-z]{2}$',
-    'time_regex': r'^\d{2}:\d{2}:\d{2}\.\d{6}$'
+    'email' : r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$',
+    'height' : r'^\d+(\.\d+)?$',
+    'snils': r'^\d{11}$',
+    'passport': r'^\d{2}\s\d{2}\s\d{6}$',
+    'occupation': r'^[А-Яа-я\s]+$',
+    'longitude': r'^-?\d+(\.\d+)?$',
+    'hex_color': r'^#[0-9A-Fa-f]{6}$',
+    'issn': r'^\d{4}-\d{4}$',
+    'locale_code': r'^[a-z]{2}-[a-z]{2}$',
+    'time': r'^\d{2}:\d{2}:\d{2}\.\d{6}$'
 }
 FILE_PATH = '35.csv'
 
-def validate_csv(file_path: str, validation_rules: dict[str]) -> List[int]:
+def validate_csv(file_path: str, validation_rules: dict[str]) -> list[int]:
     """
      Проверка строк в csv-файле. Эта функция используется для проверки строк csv с помощью регулярного выражения. Она вернет список недопустимых строк
     :параметр file_path - путь к csv-файлу, подлежащему проверке
@@ -25,7 +25,7 @@ def validate_csv(file_path: str, validation_rules: dict[str]) -> List[int]:
     """
     invalid_rows_list = []
     try:
-        df = pd.read_csv(file_path, sep=';', quotechar='"', encoding='utf-16')
+        df = pandas.read_csv(file_path, sep=';', quotechar='"', encoding='utf-16')
         for index, row in df.iterrows():
             for column, regex in validation_rules.items():
                 if not re.fullmatch(regex, str(row[column])):
