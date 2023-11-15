92fd66b2e4efb453dbbc4e08aed6c12c27786a2f
Gironape <andreyka_kazakov_03@mail.ru>
2023-11-16 01:33:06 +0400

    Функции
    Создал функции для выполнения задания

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import csv
+import os
+import re
+
+from checksum import serialize_result, calculate_checksum
 
 PATTERNS = {
     "email": "[a-zA-Z0-9._%+-]+@+[a-zA-Z0-9._%+-]+.[a-z]",
@@ -11,6 +15,46 @@ PATTERNS = {
     "isbn": "(\\d+-){4}\\d+",
     "uuid": "[a-f0-9]{8}([-a-f0-9]{4}){4}[a-f0-9]{12}",
     "time": "([01]\\d|2[0-3]):([0-5]\\d):([0-5]\\d)\\.(\\d{1,6})"
-
 }
+
+
+def is_valid_line(row: list[str]) -> bool:
+    """
+    функция которая проверяет: соответствует ли каждое значение в строке
+    шаблону в PATTERNS.
+    :param row: list[str]
+    :return: bool
+    """
+    flag = True
+    for k, v in zip(PATTERNS.keys(), row):
+        if not re.match(PATTERNS[k], v):
+            flag = False
+            return flag
+    return flag
+
+
+def get_invalid_lines(csv_file: str) -> list[int]:
+    """
+    функция, которая возвращает список индексов строк CSV-файла, не соответствующих ожидаемым шаблонам.
+    :param csv_file: str
+    :return: list[int]
+    """
+    lst = []
+    data = []
+    with open(csv_file, "r", newline="", encoding="utf-16") as file:
+        read_data = csv.reader(file, delimiter=";")
+        for row in read_data:
+            data.append(row)
+    data.pop(0)
+    for i, row in enumerate(data):
+        if not is_valid_line(row):
+            lst.append(i)
+    return lst
+
+
+if __name__ == "__main__":
+    csv_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "53.csv")
+    erroneous_lines = get_invalid_lines(csv_file)
+    print(erroneous_lines)
+    serialize_result(53, calculate_checksum(erroneous_lines))
 
