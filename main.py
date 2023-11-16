2f82c605b4264d960c4d755c36b2a618c23fe72f
Andrei <marinin1337@gmail.com>
2023-11-16 21:58:34 +0400

    Добавлен метод сериализации

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import csv
 import logging
 import re
+from checksum import calculate_checksum
+from checksum import serialize_result
 
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
 TABLE_PATH = "12.CSV"
 PATTERNS = {
     "telephone": "^\\+7-\\(\\d{3}\\)-\\d{3}-\\d{2}-\\d{2}$",
@@ -15,13 +20,14 @@ PATTERNS = {
     "locale_code": "^([a-z]{2,3})(?:-([a-zA-Z]{2,4}))?(?:-([a-zA-Z0-9-]+))?$",
     "date": "^(?:19|20)\\d\\d-(?:0[1-9]|1[012])-(?:0[1-9]|[12]\\d|3[01])$"
 }
+VARIANT = 12
 
 
 def read_csv(path: str) -> list[list[str]]:
     """
-
-    :param path:
-    :return:
+    Функция считывает данные из csv файла
+    :param path: путь к файлу
+    :return: список строк таблицы
     """
     rows = []
     try:
@@ -36,6 +42,11 @@ def read_csv(path: str) -> list[list[str]]:
 
 
 def check_row(row: list) -> bool:
+    """
+    Функция проверяет строку таблицы на наличие неправильной записи
+    :param row: строка таблицы
+    :return: Истина, если неправильных записей в строке нет. Ложь - в противном случае
+    """
     keys = list(PATTERNS.keys())
     for i in range(len(keys)):
         flag = bool(re.match(PATTERNS[keys[i]], row[i]))
@@ -45,12 +56,20 @@ def check_row(row: list) -> bool:
 
 
 def find_invalid_rows(path: str) -> list[int]:
+    """
+    Функция нахождения номеров строк, в которых присутствует неправильная запись
+    :param path: путь к таблице
+    :return: список номеров неправильных строк
+    """
     invalid_rows = []
     data = read_csv(path)
     for i in range(len(data)):
         if not check_row(data[i]):
             invalid_rows.append(i)
     return invalid_rows
+
+
 if __name__ == "__main__":
-    array = find_invalid_rows(TABLE_PATH)
-    print(len(array))
+    invalid_numbers = find_invalid_rows(TABLE_PATH)
+    hash_sum = calculate_checksum(invalid_numbers)
+    serialize_result(VARIANT, hash_sum)
