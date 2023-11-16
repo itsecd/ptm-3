fd16d3089b32a44c67fbea1beb66f1587876b0e8
Gironape <andreyka_kazakov_03@mail.ru>
2023-11-16 04:59:11 +0400

    Еще попытка?
    Алохомора

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import re
 from checksum import serialize_result, calculate_checksum
 
 PATTERNS = {
-    "email": "[a-zA-Z0-9._%+-]+@+[a-zA-Z0-9._%+-]+.[a-z]",
-    "http_status_message": "\\d{3} +[a-zA-Z ]{1,}",
-    "inn": "\\d{12}",
-    "passport": "\\d{2}\\s\\d{2}\\s\\d{6}",
-    "ip_v4": "\\d{1,3}(\\.+\\d{1,3}){3}",
-    "latitude": "-?\\d{1,2}\\.\\d{4,}",
-    "hex_color": "#[a-f0-9]{6}",
-    "isbn": "(\\d+-){4}\\d+",
-    "uuid": "[a-f0-9]{8}([-a-f0-9]{4}){4}[a-f0-9]{12}",
-    "time": "([01]\\d|2[0-3]):([0-5]\\d):([0-5]\\d)\\.(\\d{1,6})"
+    "email": r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b',
+    "http_status_message": r'\b\d{3}\b',
+    "inn": r'\d{12}',
+    "passport": r'\b\d+\s\d+\s\d+\b',
+    "ip_v4": r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b',
+    "latitude": r'[-+]?\d+\.\d+',
+    "hex_color": r'#[a-fA-F0-9]{6}\b',
+    "isbn":  r'\b\d{1,3}(?:-?\d){1,5}-?\d{1,7}-?\d\b',
+    "uuid": r'\b[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\b',
+    "time": r'\b(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d\b'
 }
 
 
@@ -29,6 +29,7 @@ def is_valid_line(row: list[str]) -> bool:
     for k, v in zip(PATTERNS.keys(), row):
         if not re.match(PATTERNS[k], v):
             flag = False
+            print(k)
             return flag
     return flag
 
@@ -56,5 +57,6 @@ if __name__ == "__main__":
     csv_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "53.csv")
     erroneous_lines = get_invalid_lines(csv_file)
     print(erroneous_lines)
+    print(len(erroneous_lines))
     serialize_result(53, calculate_checksum(erroneous_lines))
 
