92431e1b8ae91691e67107438c37dc9884b7433a
AvtoBBus <113329450+AvtoBBus@users.noreply.github.com>
2023-11-18 12:45:59 +0400

    docstrings and some fix

--- check_cols_func.py
+++ check_cols_func.py
@@ -2,11 +2,19 @@ import re
 
 
 def check_data_col(input_date: str) -> bool:
+    '''
+    Проверка даты на валидность (чтобы не было 13 месяца в году и тд.)
+    :params input_date: input date needed to validate
+    '''
     date = list(map(int, re.findall(r'\d+', input_date)))
-    return 0 < date[0] < 2025 and 0 < date[1] < 13 and 0 < date[2] < 32
+    return 0 < date[0] < 2125 and 0 < date[1] < 13 and 0 < date[2] < 32
 
 
 def check_ipv4(input_ip: str) -> bool:
+    '''
+    Проверка ip_v4 на валидность
+    :params input_ip: input ip_v4 needed to validate
+    '''
     numbers = list(map(int, re.findall(r'\d{1,3}', input_ip)))
     for number in numbers:
         if number > 255:
@@ -14,5 +22,5 @@ def check_ipv4(input_ip: str) -> bool:
     return True
 
 
-def check_longitude(input_longitude: str) -> bool:
-    return float(input_longitude) > -180.0 and float(input_longitude) < 180.0
+def check_longitude(input_longitude): return abs(
+    float(input_longitude)) < 180.0
--- main.py
+++ main.py
@@ -12,6 +12,11 @@ PATTERNS_PATH = "patterns.json"
 
 
 def read_patterns(path_to_file: str) -> json:
+    '''
+    Считывание паттернов из файла path_to_file
+    :params path_to_file: path to file with patterns
+    :return: json file with patterns
+    '''
     patterns = []
     with open(path_to_file, 'r') as fp:
         patterns = json.load(fp)
@@ -19,6 +24,14 @@ def read_patterns(path_to_file: str) -> json:
 
 
 def check_column(dataset: pd.DataFrame, column: str, pattern: str, check_correct: Callable[[str], bool] = lambda x: True) -> list[int]:
+    '''
+    Проверка конкретного column в dataset на соответствие pattern, при необходимости проверяется значение ф-цией check_correct
+    :params dataset: dataset to search incorrect cells
+    :params column: column of dataset's
+    :params pattern: pattern that use to search
+    :params check_correct: function needed to check some value like date or degree
+    :return: found indexes
+    '''
     result = []
     for i in range(len(dataset[column])):
         if not re.fullmatch(pattern, dataset[column][i], re.X) or not check_correct(dataset[column][i]):
@@ -28,9 +41,12 @@ def check_column(dataset: pd.DataFrame, column: str, pattern: str, check_correct
     return result
 
 
-if __name__ == "__main__":
-    os.system("cls")
-    dataset = pd.read_csv(CSV_PATH, sep=";", encoding="utf-16")
+def start_check(dataset: pd.DataFrame) -> set:
+    '''
+    Начало проверки dataset
+    :params dataset: dataset to check
+    :return: set of list with found indexes
+    '''
     patterns = []
     patterns = read_patterns(PATTERNS_PATH)
     result = set()
@@ -46,5 +62,12 @@ if __name__ == "__main__":
                 dataset, col, patterns[col], check_longitude))
         else:
             result.update(check_column(dataset, col, patterns[col]))
+    return result
+
+
+if __name__ == "__main__":
+    os.system("cls")
+    dataset = pd.read_csv(CSV_PATH, sep=";", encoding="utf-16")
+    result = start_check(dataset)
     print(calculate_checksum(list(result)))
     serialize_result(VARIANT, calculate_checksum(list(result)))
