b7bff5993e1488cd91a8d4ac9e3f99e7634cd070
kseniaDubova <ksushadubova@icloud.com>
2023-11-18 21:26:10 +0400

    изменение patterns, запись cheksum

--- main.py
+++ main.py
@@ -3,38 +3,36 @@ import re
 
 from checksum import calculate_checksum, serialize_result
 
-# "email";"height";"inn";"passport";"occupation";"latitude";"hex_color";"issn";"uuid";"time"
+VARIANT = 7
+
 PATTERNS = {
     "email": "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
-    "height": "^\d\.\d{2}$",
-    "inn": "\d{12}",
-    "passport": "^\d{2}\s\d{2}\s\d{6}$",
+    "height": "^(?:0|1|2)\.\d{2}$",
+    "inn": "^\d{12}$",
+    "passport": "^\d{2} \d{2} \d{6}$",
     "occupation": "^[a-zA-Zа-яА-ЯёЁ\s-]+$",
-    "latitude": "-?(?:90(?:\.0+)?|[1-8]?\d(?:\.\d+)?)",
-    "hex_color": "^#([a-f0-9]{6}|[a-f0-9]{3})$",
+    "latitude": "^(-?[1-8]?\d(?:\.\d{1,})?|90(?:\.0{1,})?)$",
+    "hex_color": "^#[0-9a-fA-F]{6}$",
     "issn": "^\d{4}-\d{4}$",
-    "uuid": "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
+    "uuid": "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$",
     "time": "^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)\.(\d{1,6})$"
 }
 
 
-def cheak_invalid_latitude(item: str) -> bool:
-    if not re.match(PATTERNS["latitude"], item) or re.search("[^0-9.+-]", item):
-        return False
-    return float(item) > -90 or float(item) < 90
-
-
 def check_invalid_row(row: list) -> bool:
-    flag = True
+    '''
+        Функция проверки каждой строки на валидность
+    '''
     for patterns, item in zip(PATTERNS.keys(), row):
-        if patterns == "latitude":
-            flag = cheak_invalid_latitude(item)
-        elif not re.search(PATTERNS[patterns], item):
+        if not re.search(PATTERNS[patterns], item):
             return False
-    return flag
+    return True
 
 
 def get_no_invalid_data_index(data: list) -> list:
+    '''
+        Функция находит невалидные строки и записывает их индексы 
+    '''
     data_index = []
     index = 0
     for row in data:
@@ -45,10 +43,14 @@ def get_no_invalid_data_index(data: list) -> list:
 
 
 if __name__ == "__main__":
+    '''
+        Считывание csv файла и запись checksum
+    '''
     data = []
     with open("7.csv", "r", newline="", encoding="utf-16") as file:
         read_data = csv.reader(file, delimiter=";")
         for row in read_data:
             data.append(row)
     data.pop(0)
-    print(len(get_no_invalid_data_index(data)))
+    serialize_result(VARIANT, calculate_checksum(
+        get_no_invalid_data_index(data)))
