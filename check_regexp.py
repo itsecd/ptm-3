bed975854ababae37c9b44b878ebbae0b12614af
Степан Семыкин <semykinstepan2@gmail.com>
2023-11-27 22:40:32 +0400

    Создана функция для проверки регулярных выражений

--- check_regexp.py
+++ check_regexp.py
@@ -1,5 +1,7 @@
 import csv
 import json
+import re
+
 
 def read_csv(path_to_csv: str) -> list:
     try:
@@ -16,6 +18,7 @@ def read_csv(path_to_csv: str) -> list:
         print(err)    
     return rows_csv
 
+
 def read_json(path_to_json: str) -> dict:
     try:
         with open(path_to_json, "r") as f:
@@ -27,3 +30,12 @@ def read_json(path_to_json: str) -> dict:
     except Exception as err:
         print(err)          
     return reader
+
+
+def check_rows(rows_list: list, regexp_dict: dict) -> list:
+    number_invalid_rows = []
+    for key, value in regexp_dict.items():
+        for index, row in enumerate(rows_list):
+            if not re.match(value, row[key]):
+                number_invalid_rows.append(index)
+    return number_invalid_rows    
