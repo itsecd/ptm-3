a254200c7da4475d4e2007271b90ae46ad06d044
K0tlyarski <djgame2.9.6@gmail.com>
2023-11-29 18:04:38 +0400

    added a file with regular expressions

---
+++ main.py
@@ -0,0 +1,8 @@
+import csv
+import json
+import re
+from checksum import serialize_result, calculate_checksum
+
+CSV_PATH = "/Users/vadimkotlarskij/Desktop/Python/ptm-3/29.csv"
+REG_JSON_PATH = "/Users/vadimkotlarskij/Desktop/Python/ptm-3/reg.json"
+
