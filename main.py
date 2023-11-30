f4e715634bf464bfa06d88e461db2f13791d80be
Egor Sysolyatin <113940124+RYTpL@users.noreply.github.com>
2023-11-30 22:22:26 +0400

    regexes to module constants.

--- main.py
+++ main.py
@@ -2,12 +2,31 @@ import csv
 import re
 import checksum
 
+REGEX_PATTERNS = {
+    "email": re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"),
+    "http_status_message": re.compile(
+        r"(([1-9])|([1-9][0-9])|([1-9][0-9][0-9]))\s[A-Za-z ]"
+    ),
+    "snils": re.compile(r"^[0-9]{11}$"),
+    "passport": re.compile(r"([0-9]{2}\s){2}[0-9]{6}"),
+    "ip_v4": re.compile(
+        r"^((25[0-5]|2[0-4]\d|1\d\d|\d?\d)\.){3}(25[0-5]|2[0-4]\d|1\d\d|\d?\d)$"
+    ),
+    "longitude": re.compile(r"^-?((1[0-7]\d|\d?\d)(?:\.\d{1,})?|180(\.0{1,})?)$"),
+    "hex_color": re.compile(r"\#[0-9a-fA-F]{6}$"),
+    "isbn": re.compile(
+        r"[0-9]{3}-[0-9]{1}-[0-9]{5}-[0-9]{3}-[0-9]{1}|[0-9]{1}-[0-9]{5}-[0-9]{3}-[0-9]{1}"
+    ),
+    "locale_code": re.compile(r"[a-z]{2}(-([a-z]){2})?$"),
+    "time": re.compile(r"([0-1][0-9]|2[0-3])\:[0-5][\d]\:[0-5][\d]\.[\d]{6}"),
+}
+
 
 def read_file(filename: str) -> list:
     """
-    read file and send every string to check on valid regualr
+    Read file and check each string for valid regular expressions.
     filename - name of our variant
-    return list_for_checksum - list of indexstring where regular not matched
+    return list_for_checksum - list of index strings where regular expression is not matched
     """
     first_string = True
     title = ""
@@ -30,34 +49,15 @@ def read_file(filename: str) -> list:
 def check(string_dict: dict) -> bool:
     """
     string_dict - dict in which key is title and value is current word of string
-    return True if value is not matched with regular
+    return True if value does not match the regular expression
     """
-    valid = {
-        "email": re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"),
-        "http_status_message": re.compile(
-            r"(([1-9])|([1-9][0-9])|([1-9][0-9][0-9]))\s[A-Za-z ]"
-            ),
-        "snils": re.compile(r"^[0-9]{11}$"),
-        "passport": re.compile(r"([0-9]{2}\s){2}[0-9]{6}"),
-        "ip_v4": re.compile(
-            r"^((25[0-5]|2[0-4]\d|1\d\d|\d?\d)\.){3}(25[0-5]|2[0-4]\d|1\d\d|\d?\d)$"
-        ),
-        "longitude": re.compile(r"^-?((1[0-7]\d|\d?\d)(?:\.\d{1,})?|180(\.0{1,})?)$"),
-        "hex_color": re.compile(r"\#[0-9a-fA-F]{6}$"),
-        "isbn": re.compile(
-            r"[0-9]{3}-[0-9]{1}-[0-9]{5}-[0-9]{3}-[0-9]{1}|[0-9]{1}-[0-9]{5}-[0-9]{3}-[0-9]{1}"
-        ),
-        "locale_code": re.compile(r"[a-z]{2}(-([a-z]){2})?$"),
-        "time": re.compile(r"([0-1][0-9]|2[0-3])\:[0-5][\d]\:[0-5][\d]\.[\d]{6}"),
-    }
-
     for title, value in string_dict.items():
-        if re.match(valid[title], value) == None:
-            return True  # if we found a  not validation value
+        if re.match(REGEX_PATTERNS[title], value) is None:
+            return True  # if we found a not validation value
     return False
 
 
 if __name__ == "__main__":
     list_of_index_no_valid_strings = read_file("41.csv")
-    hash = checksum.calculate_checksum(list_of_index_no_valid_strings)
-    checksum.serialize_result(41, hash)
+    hash_value = checksum.calculate_checksum(list_of_index_no_valid_strings)
+    checksum.serialize_result(41, hash_value)
