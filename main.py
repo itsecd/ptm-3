7ac48d47f6bc6feed2068843d906e6f372ab1cca
Маргарита <113328619+MaargoGysarova@users.noreply.github.com>
2023-12-01 22:52:49 +0400

    Доработала и получила рузльтат

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ VARIANT = 6
 PATH_TO_CSV = "6.csv"
 
 PATTERNS = {
-    "telephone" : r"(\d{3})-(\d{3})-(\d{4})",
+    "telephone": "^\\+7-\\(\\d{3}\\)-\\d{3}-\\d{2}-\\d{2}$",
     "http_status_message" : "^\\d{3}\\s[^\n\r]+$",
-    "inn" : r"(\d{10})",
-    "identifier" : r"(\d{15})",
+    "inn": "^\\d{12}$",
+    "identifier": "^\\d{2}-\\d{2}/\\d{2}$",
     "ip_v4": "^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$",
-    "latitude": r"(\d{2}\.\d{6})",
-    "blood_type": r"(A|B|AB|0)[+-]",
+    "latitude": "^(?:\\+|-|)(90\\.0+|[0-8]\\d\\.\\d+|\\d\\.\\d+|0+\\d\\.\\d+)$",
+    "blood_type": "^(?:O|A|B|AB)[\\+\u2212]$",
     "isbn": "\\d+-\\d+-\\d+-\\d+(:?-\\d+)?$",
-    "uuid": r"([a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12})",
-    "date": r"(\d{2})\.(\d{2})\.(\d{4})",
+    "uuid": "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$",
+    "date": "^(?:19|20)\\d\\d-(?:0[1-9]|1[012])-(?:0[1-9]|[12]\\d|3[01])$"
 }
 
 def read_csv(path: str) -> list:
     data = []
-    with open("7.csv", "r", newline="", encoding="utf-16") as file:
+    with open("6.csv", "r", newline="", encoding="utf-16") as file:
         read_data = csv.reader(file, delimiter=";")
         for row in read_data:
             data.append(row)
