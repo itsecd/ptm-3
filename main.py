b2b3cc7de2d45ba2e8d8eaba89c27e632c08c9ed
lenayak <113600536+lenayak@users.noreply.github.com>
2023-12-03 14:57:25 +0400

    fixed regexp

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from checksum import serialize_result, calculate_checksum
 
 PATTERNS = {
     "email": "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
-    "height": "^[1-3]\\.\\d{2}$",
+    "height": "^[1-2]\\.\\d{2}$",
     "snils": "^\\d{11}$",
     "passport": "^\\d{2}\\s\\d{2}\\s\\d{6}$",
     "occupation": "^[a-zA-Zа-яА-ЯёЁ\\s-]+$",
