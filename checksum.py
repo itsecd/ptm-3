a52d0003159bdf467350516cc9e201d73793827c
Colnuwko <Vladislav290403@gmail.com>
2023-12-03 10:44:21 +0400

    Урал неиспользуемый код

--- checksum.py
+++ checksum.py
@@ -23,8 +23,6 @@ def calculate_checksum(row_numbers: List[int]) -> str:
     :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
     :return: md5 хеш для проверки через github action
     """
-    # print(row_numbers)
-    # print(len(row_numbers), type(row_numbers[0]))
     row_numbers.sort()
     return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
 
