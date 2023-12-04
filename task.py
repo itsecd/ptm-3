45b7e2b166bea6d7c15f2ce880db7d955f892229
Mavey <rolanddiskein666@mail.ru>
2023-12-04 19:49:33 +0400

    Check_dataset
    Написана Функция для проверки данных на невалидность

--- task.py
+++ task.py
@@ -1,5 +1,5 @@
 import csv 
-
+import re 
 
 VAR=33
 PATTERNS ={
@@ -8,8 +8,10 @@ PATTERNS ={
     "snils": "^\d{11}$",
     "passport": "^\d{2} \d{2} \d{6}$",
     "ip_v4": "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",
+    "longitude": "^-?((180(\\.0+)?|1[0-7]?\\d(\\.\\d+)?|\\d{1,2}(\\.\\d+)?)|0(\\.\\d+)?)$",
     "hex_color": "^#[0-9a-fA-F]{6}$",
     "issn": "^\d{4}-\d{4}$",
+    "isbn": "\\d+-\\d+-\\d+-\\d+(:?-\\d+)?$",
     "locale_code": "^([a-z]{2,3})(?:-([a-zA-Z]{2,4}))?(?:-([a-zA-Z0-9-]+))?$",
     "time": "^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)\.(\d{1,6})$"
 }
@@ -25,6 +27,25 @@ def read_file(name=str(VAR)):
     return rows
 
 
+def check_row(row: list) -> bool:
+    for patterns, item in zip(PATTERNS.keys(), row):
+        if not re.search(PATTERNS[patterns], item):
+            return False
+    return True
+
+
+def get_index_invalid_data(data):
+    index_data = []
+    i = 0
+    for row in data:
+        if(check_row(row) == False):
+            index_data.append(i)
+        i+=1
+    return index_data
+
+
 if __name__ == "__main__":
-    data= read_file()
-    print("D")
+    data = read_file()
+    counter = get_index_invalid_data(data)
+    print(len(counter))
+    print("O")
