39d37888aad65926d924041faa4a38595db22e59
Nikita-Baldin <nikitabaldin22102003@mail.ru>
2023-12-08 01:07:38 +0400

    дабавил REGEXPS

---
+++ REGEXPS.py
@@ -0,0 +1,20 @@
+import json
+
+
+REGEXPS_PATH = "REGEXPS.json"
+REGEXPS = {
+    "email": "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+    "http_status_message": "^\d{3}\s[a-zA-Z0-9_ ]{1,}",
+    "snils": "^\\d{11}$",
+    "passport": "^\\d{2}\\s\\d{2}\\s\\d{6}$",
+    "ip_v4": "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",
+    "longitude": "^-?((180(\\.0+)?|1[0-7]?\\d(\\.\\d+)?|\\d{1,2}(\\.\\d+)?)|0(\\.\\d+)?)$",
+    "hex_color": "^#([a-f0-9]{6}|[a-f0-9]{3})$",
+    "isbn": "^\d+-\d+-\d+-\d+(:?-\d+)?$",
+    "locale_code": "^([a-z]{2,3})(?:-([a-zA-Z]{2,4}))?(?:-([a-zA-Z0-9-]+))?$",
+    "time": "^([01]\\d|2[0-3]):([0-5]\\d):([0-5]\\d)\\.(\\d{1,6})$"
+}
+
+if __name__ == "__main__":
+    with open(REGEXPS_PATH, 'w') as fp:
+        json.dump(REGEXPS, fp)
