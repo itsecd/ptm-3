8a49eb8099e2b419f61e8de97e442f0b7a018441
Vladimir <vovakarpov2017@gmail.com>
2023-12-10 17:13:36 +0400

    Added check valid data function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import csv
+import re
 
 PATTERNS = {}
+
 
 def read_csv_file(file_name: str) -> list:
     """
@@ -19,3 +21,17 @@ def read_csv_file(file_name: str) -> list:
         data.pop(0)
     return data
 
+
+def check_valid_data(data: list) -> bool:
+    """
+    Check string for data validity.
+
+    Args: A list representing the row of data to be checked.
+    
+    Return: A boolean value indicating whether the row of data is valid or not.
+    
+    """
+    for key, value in zip(PATTERNS.keys(), data):
+        if not re.match(PATTERNS[key], value):
+            return False
+    return True
