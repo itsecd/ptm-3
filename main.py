900c3875326f4276d7f0adcb61a8eb76f5bdec65
Vimslpb Obsm <global0908@mail.ru>
2023-12-14 13:20:22 +0300

    Pulled regexps into json file

--- main.py
+++ main.py
@@ -1,36 +1,46 @@
 import os
 import pandas as pd
+import json
 import re
 from checksum import calculate_checksum, serialize_result
 
 V = 23
 
-def validate_dataframe(df):
+def read_regexps(path_to_file ="regexp_me.json") -> json:
+    '''
+    Считывание regexp'ов 
+    '''
+    rs = []
+    with open(path_to_file, 'r', encoding='utf-8') as fp:
+        rs = json.load(fp)
+    return rs
+
+def validate_dataframe(df: pd.DataFrame) -> list:
+    """Валидация строк
+
+    Args:
+        df (pd.DataFrame): DataFrame
+
+    Returns:
+        list: Список номеров невалидных строк
+    """
     invalid_rows = []
+    
+    regexps = read_regexps() 
 
     # Regular expressions for validation
-    email_regex = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
-    height_regex = re.compile(r'^(?:0|1|2)\.\d{2}$')
-    inn_regex = re.compile(r'^\d{12}$')
-    passport_regex = re.compile(r'^\d{2} \d{2} \d{6}$')
-    occupation_regex = re.compile(r'^[a-zA-Zа-яА-ЯёЁ\s-]+$')
-    lattitude_regex = re.compile(r'^(-?[1-8]?\d(?:\.\d{1,})?|90(?:\.0{1,})?)$')
-    hex_color_regex = re.compile(r'^#[0-9a-fA-F]{6}$')
-    issn_regex = re.compile(r'^\d{4}-\d{4}$')
-    uuid_regex = re.compile(r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$')
-    time_regex = re.compile(r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)\.(\d{1,6})$')
 
     for index, row in df.iterrows():
-        if not (email_regex.match(row['email']) and
-                height_regex.match(str(row['height'])) and
-                inn_regex.match(str(row['inn'])) and
-                passport_regex.match(row['passport']) and
-                occupation_regex.match(row['occupation']) and
-                lattitude_regex.match(str(row['latitude'])) and # Latitude as a decimal number
-                hex_color_regex.match(row['hex_color']) and
-                issn_regex.match(row['issn']) and
-                uuid_regex.match(row['uuid']) and
-                time_regex.match(row['time'])):
+        if not (re.match(regexps["email"],row['email']) and
+                re.match(regexps["height"],row['height']) and
+                re.match(regexps["inn"],row['inn']) and
+                re.match(regexps["passport"],row['passport']) and
+                re.match(regexps["occupation"],row['occupation']) and
+                re.match(regexps["latitude"],row['latitude']) and # Latitude as a decimal number
+                re.match(regexps["hex_color"],row['hex_color']) and
+                re.match(regexps["issn"],row['issn']) and
+                re.match(regexps["uuid"],row['uuid']) and
+                re.match(regexps["time"],row['time'])):
             invalid_rows.append(index)
 
     return invalid_rows
