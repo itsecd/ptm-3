3b8b6b24ad1d322c0bbf342d020ae11c53f69dea
Oleg <olegbabi4ev@mail.ru>
2023-12-20 19:09:44 +0400

    исправление результатов

--- regular_expressions.py
+++ regular_expressions.py
@@ -4,9 +4,9 @@ regexps = {
     "snils": r"^\d{11}$",
     "identifier": r"^\d{2}-\d{2}/\d{2}$",
     "occupation": r"^[А-Я]?[а-я]+((?:\s|-)[а-я]+)*$",
-    "longitude": r"^[+-]?\d{,3}\.\d{6}$",
-    "blood_type": r"^(?:[AB]{,2}|O)[+-]?$",
-    "issn": r"^\d{4}\-\d{4}$",
+    "longitude": r"^-?\d{0,3}\.\d{6}$",
+    "blood_type": r"^(?:[AB]{,2}|O)[+−]?$",
+    "issn": r"^\d{4}\-\d{4}$",  
     "locale_code": r"^[a-z]{1}(?:\-[a-z])?$",
     "date": r"^\d{4}\-\d{2}\-\d{2}$"
 }
